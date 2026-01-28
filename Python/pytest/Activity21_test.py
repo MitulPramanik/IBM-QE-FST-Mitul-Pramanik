e1cf09903dd0a901f886b8850712db0be6d8e246
Mitul Pramanik <104298870+MitulPramanik@users.noreply.github.com>
2026-01-28 12:29:48 +0530

    Add files via upload

---
+++ Python/pytest/Activity21_test.py
@@ -0,0 +1,23 @@
+import pytest
+import math
+
+def test_sum():
+    num1 = 25
+    num2 = 25
+    assert num1+num2 == 5
+
+def test_diff():
+    num1 =25
+    num2 =10
+    assert num1 -num2 == 15
+
+def test_quotient():
+    num1 = 20
+    num2 = 5
+    assert num1 / num2 == 4
+
+def test_mult():
+    num1 =25
+    num2 = 2
+    assert num1*num2 == 50
+    
