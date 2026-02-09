83395e064017cedab6fe98641d0c9f43086b8c53
Mitul Pramanik <104298870+MitulPramanik@users.noreply.github.com>
2026-02-09 14:24:49 +0530

    Add files via upload

---
+++ cucumber/Activity2Steps.java
@@ -0,0 +1,46 @@
+package StepDefinitions;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Activity2Steps extends BaseClass {
+    @Given("the user is on the login page")
+    public void openPage() {
+        // Open the login page
+        driver.get("https://training-support.net/webelements/login-form");
+        // Assert page title
+        Assertions.assertEquals("Selenium: Login Form", driver.getTitle());
+          throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters username and password")
+    public void enterCredentials() {
+        // Find username field and enter username
+        driver.findElement(By.id("username")).sendKeys("admin");
+        // Find password field and enter password
+        driver.findElement(By.id("password")).sendKeys("password");
+          throw new io.cucumber.java.PendingException();
+    }
+
+    @And("clicks the submit button")
+    public void clickSubmit() {
+        // Find the submit button and click it
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+          throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("get the confirmation message and verify it")
+    public void confirmMessage() {
+        // Find the confirmation message
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("h2.mt-5"), "Welcome"));
+        String message = driver.findElement(By.cssSelector("h2.mt-5")).getText();
+        // Assert message
+        Assertions.assertEquals("Welcome Back, Admin!", message);
+          throw new io.cucumber.java.PendingException();
+    }
+}
